cd82c266ee606017eb83d61e6b5a376ab6684c03
Guiu Riera <guiur@hotmail.com>
2026-06-18 10:33:49 +0200

    Initial project setup

---
+++ main.py
@@ -0,0 +1,11 @@
+from dotenv import load_dotenv, find_dotenv
+
+
+load_dotenv(find_dotenv(".env"))
+
+def main():
+    print("Hello from udemyagenticrag!")
+
+
+if __name__ == "__main__":
+    main()
