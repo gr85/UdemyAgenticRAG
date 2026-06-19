cdbf5342f1986f319a13f36ed85cb6424dbcb627
Guiu Riera <guiur@hotmail.com>
2026-06-19 11:03:00 +0200

    Add generation node and chain and a sanity test check

--- graph/chains/tests/test_chains.py
+++ graph/chains/tests/test_chains.py
@@ -1,6 +1,8 @@
 from dotenv import load_dotenv, find_dotenv
+from pprint import pprint
 
 from graph.chains.retrieval_grader import GradeDocuments, retrieval_grader
+from graph.chains.generation import generation_chain
 from ingestion import retriever
 
 load_dotenv(find_dotenv(".env"))
@@ -25,3 +27,10 @@ def test_retrieval_grader_answer_no() -> None:
     )
 
     assert res.binary_score == "no"
+
+
+def test_generation_chain() -> None:
+    question = "agent memory"
+    docs = retriever.invoke(question)
+    generation = generation_chain.invoke({"context": docs, "question": question})
+    pprint(generation)
